a99b2784765d9967b17d4dc1e6cd1b23199061d2
sagebeme <eudicas5k@gmail.com>
2022-11-16 03:18:59 -0800

    passing betty

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * @ca: count arg
+ * @va: vector arg
+ *
+ * Return: 0 on success, 1 on error
+ */
+
+int main(int ca, char **av)
+{
+	char *prompt = "(Type here) $ ";
+	size_t promptlen = 0;
+	write(STDIO_FILENO, prompt, 12);
+
+	getline(&prompt, &promptlen, stdin);
+
+	write(STDIO_FILENO, prompt, strlen(promptlen));
+
+	return(0);
+}
---
+++ shell.h
@@ -0,0 +1,9 @@
+#ifndef _shell_h
+#define _shell_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#endif /* _shell_h*/
