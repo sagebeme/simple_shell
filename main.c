b98ca31e973cca562e766119a5e7d799c1f3ad6c
sagebeme <eudicas5k@gmail.com>
2022-11-16 03:26:48 -0800

    betty

--- main.c
+++ main.c
@@ -8,15 +8,15 @@
  * Return: 0 on success, 1 on error
  */
 
-int main(int ca, char **av)
+int main(int ca, char **va)
 {
 	char *prompt = "(Type here) $ ";
 	size_t promptlen = 0;
+
 	write(STDIO_FILENO, prompt, 12);
 
 	getline(&prompt, &promptlen, stdin);
-
 	write(STDIO_FILENO, prompt, strlen(promptlen));
 
-	return(0);
+	return (0);
 }
